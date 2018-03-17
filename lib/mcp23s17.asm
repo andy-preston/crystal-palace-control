3c13355129e4c8764d6184a0a6e785f66f96d91f
Andy Preston <edgeeffect@gmail.com>
2018-03-17 19:00:09 +0000

    untested MCP23S17 macros

---
+++ lib/mcp23s17.asm
@@ -0,0 +1,17 @@
+; parameters @0 - chip, @1 - register, @2 - value
+
+.MACRO mcp23s17Read
+    chipSelectI @0
+    spiOutI OP_READ
+    spiOutI @1
+    spiOutIn 0, @2
+    chipDeselect
+.ENDMACRO
+
+.MACRO mcp23s17Write
+    chipSelectI @0
+    spiOutI OP_WRITE
+    spiOutI @1
+    spiOutI @2
+    chipDeselect
+.ENDMACRO
