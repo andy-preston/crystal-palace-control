15891b4e432a5a79dc31520997acae6de8a3cd2d
Andy Preston <edgeeffect@gmail.com>
2018-05-05 13:44:08 +0100

    new register assignments

--- lib/registers.asm
+++ lib/registers.asm
@@ -1,12 +1,20 @@
+; only registers r16-r31 can LDI
+
 .DEF addrLReg = r16
 .DEF addrHReg = r17
+.DEF stringLReg = r18
+.DEF stringHReg = r19
+.DEF valReg = r20     ; value to write to an IO chip register
+.DEF regReg = r21     ; register in a IO chip
+.DEF spiReg = r22     ; SPI internal operations
+.DEF portReg = r23    ; value to write to AVR port
+.DEF quickReg = r24   ; very short intermediate values
+.DEF countReg = r25
 
-.DEF valReg = r21 ; value to write to an IO chip register
-.DEF regReg = r22 ; register in a IO chip
-.DEF spiReg = r23
-.DEF portReg = r24
-.DEF quickReg = r25
-
-; ZL and ZH don't seem to get defined in my version of GAVRASM
+; XL, XH, YL, YH, ZL, ZH don't seem to get defined in my version of GAVRASM
+.DEF XL = r26
+.DEF XH = r27
+.DEF YL = r28
+.DEF YH = r29
 .DEF ZL = r30
 .DEF ZH = r31
